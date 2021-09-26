159f3dc850b03bb05268da1d0ff8b9728aaef7e8
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2021-09-26 12:04:14 +0300

    second

--- app/src/main/java/com/example/bluetoothkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothkotlin/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.bluetoothkotlin
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
+import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,5 +61,36 @@ class MainActivity : AppCompatActivity() {
         unregisterReceiver(receiver)
     }
 
+    private inner class ConnectThread(device: BluetoothDevice) : Thread() {
+
+        val myUUID: UUID = UUID.randomUUID()
+
+        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            device.createRfcommSocketToServiceRecord(myUUID)
+        }
+
+        public override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter?.cancelDiscovery()
+
+            mmSocket?.let { socket ->
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                socket.connect()
+
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+                //manageMyConnectedSocket(socket)
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmSocket?.close()
+            } catch (e: IOException) {
+            }
+        }
+    }
 
 }
