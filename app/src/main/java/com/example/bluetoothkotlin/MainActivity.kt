82af6728efe80a6c6bd29e0d49d3019cc08099f2
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2021-09-26 18:23:49 +0300

    Отлажено соединение блютуз

--- app/src/main/java/com/example/bluetoothkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothkotlin/MainActivity.kt
@@ -8,9 +8,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import java.io.IOException
 import java.util.*
@@ -19,22 +19,40 @@ class MainActivity : AppCompatActivity() {
 
     val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
+    val bluetoothAddress = "98:D3:31:F9:8D:32"
+
+    private lateinit var myDevice: BluetoothDevice
+    var founded = false
+
+    private lateinit var connectionThread: ConnectThread
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Запрос разрешений
+        requestPermissions()
+
+        //Регистрация ресивера
+        registerMyReceiver()
+    }
+
+    private fun requestPermissions() {
         // Запрос разрешения на использование геолокации
         ActivityCompat.requestPermissions(this,
             arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.BLUETOOTH,
                 Manifest.permission.BLUETOOTH_ADMIN),
             1 )
+    }
 
+    private fun registerMyReceiver() {
         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
         registerReceiver(receiver, filter)
     }
 
+    //Инициализация широковещательного приемника
     private val receiver = object  : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val action: String? = intent?.action
@@ -44,32 +62,28 @@ class MainActivity : AppCompatActivity() {
                     val deviceName = device?.name
                     val deviceHardwareAddress = device?.address
                     println("Name: $deviceName. Address: $deviceHardwareAddress")
+                    if (device?.address.equals(bluetoothAddress) && !founded) {
+                        if (device != null) {
+                            myDevice = device
+                            founded = true
+                            Toast.makeText(context, "Устройство найдено", Toast.LENGTH_SHORT).show()
+                        }
+                    }
                 }
             }
         }
 
     }
 
-    fun click(view: android.view.View) {
-
-        bluetoothAdapter?.startDiscovery()
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(receiver)
-    }
-
+    //Класс соединения блютуз
     private inner class ConnectThread(device: BluetoothDevice) : Thread() {
-
-        val myUUID: UUID = UUID.randomUUID()
+        val myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
         private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
             device.createRfcommSocketToServiceRecord(myUUID)
         }
 
-        public override fun run() {
+        override fun run() {
             // Cancel discovery because it otherwise slows down the connection.
             bluetoothAdapter?.cancelDiscovery()
 
@@ -93,4 +107,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Кнопка начала поиска устройств
+    fun click(view: android.view.View) {
+
+        bluetoothAdapter?.startDiscovery()
+
+    }
+
+    //Кнопка запуска соединения
+    fun connect(view: android.view.View) {
+        //Создать поток соединения и запускить
+        if (myDevice != null) {
+            connectionThread = ConnectThread(myDevice)
+            connectionThread.start()
+            Toast.makeText(this, "Попытка соединения", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Device = null", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //Отказ от регистрации приемника при закрытии приложения
+        unregisterReceiver(receiver)
+    }
 }
